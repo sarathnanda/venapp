596ad35d8dda493e2d4bf5527d68d95b699f62fc
sarathnanda <sarathnanda460@gmail.com>
2017-04-12 06:56:37 +0530

    4th commit

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -1,12 +1,25 @@
 package com.app;
 
 public class Employee {
-	private int empName;
-
+	public int empId;
+	private double empSal;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
 	@Override
 	public String toString() {
-		return "Employee [empName=" + empName + "]";
+		return "Employee [empId=" + empId + ", empSal=" + empSal + "]";
 	}
+
 	
 	
 
